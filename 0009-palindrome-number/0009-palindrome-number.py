2abfa7bc5bb78e53a0f7af8586946e2d0b03b433
Ghufran Ahmed Barcha <128409464+GhufranBarcha@users.noreply.github.com>
2025-01-14 23:54:28 +0500

    Time: 12 ms (29.2%), Space: 17.8 MB (18.48%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,8 +1,21 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         ## Brute force
+    #    return True if  str(x)[::-1] == str(x) else False
 
-       return True if  str(x)[::-1] == str(x) else False
+        ## Efficient way
+
+        if x < 0:
+            return False
+        reverse = 0
+        xcopy = x
+
+        while x > 0:
+            reverse = (reverse * 10) + (x % 10)
+            x = x // 10
+
+        return reverse == xcopy    
+        
        
 
 
