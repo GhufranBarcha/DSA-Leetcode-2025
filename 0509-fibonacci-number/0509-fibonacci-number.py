73c0cdcd39d00eafb9ae3f2026d388c9d3a54e67
Ghufran Ahmed Barcha <128409464+GhufranBarcha@users.noreply.github.com>
2025-01-30 12:00:53 +0500

    Time: 680 ms (12.81%), Space: 12.4 MB (37.64%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,16 +1,13 @@
-class Solution:
-    def fib(self, n: int) -> int:
-        def fibna(n):
-            if n == 1:
-                return 1
-            if n == 0:
-                return 0  
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
 
-            return fibna(n-1) + fibna(n-2)
-
-        if n>=0 and n<=30:
-            return fibna(n)  
-        else: return -1      
-
-
+        return self.fib(n - 1) + self.fib(n - 2)        
         
